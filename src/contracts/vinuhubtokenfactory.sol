1551238d159fcdfb001151dfbe05f829094542ca
VINUHUB <aladoemeka1@gmail.com>
2025-11-18 03:28:02 -0800

    Update vinuhubtokenfactory.sol

--- src/contracts/vinuhubtokenfactory.sol
+++ src/contracts/vinuhubtokenfactory.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.20;
 contract TokenFactory {
     uint256 public creationFee = 10000 ether;
     address public owner;
+    address public feeWallet;
 
-    event TokenCreated(address indexed tokenAddress, address indexed creator);
+    event TokenCreated(address indexed token, address indexed creator);
+    event FeeWithdrawn(address indexed to, uint256 amount);
+    event FeeWalletUpdated(address indexed oldWallet, address indexed newWallet);
+    event CreationFeeUpdated(uint256 oldFee, uint256 newFee);
 
-    constructor() {
+    constructor(address _feeWallet) {
+        require(_feeWallet != address(0), "Invalid fee wallet");
         owner = msg.sender;
+        feeWallet = _feeWallet;
     }
 
     modifier onlyOwner() {
@@ -17,45 +23,52 @@ contract TokenFactory {
     }
 
     function setCreationFee(uint256 _fee) external onlyOwner {
+        emit CreationFeeUpdated(creationFee, _fee);
         creationFee = _fee;
+    }
+
+    function setFeeWallet(address _newWallet) external onlyOwner {
+        require(_newWallet != address(0), "Zero address");
+        emit FeeWalletUpdated(feeWallet, _newWallet);
+        feeWallet = _newWallet;
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds");
+        (bool sent, ) = feeWallet.call{value: balance}("");
+        require(sent, "Withdraw failed");
+        emit FeeWithdrawn(feeWallet, balance);
     }
 
     function createToken(
         string memory name,
         string memory symbol,
-        uint256 initialSupply,
+        uint256 supply,
         uint8 decimals,
-        uint256 buyTaxRate,
-        uint256 sellTaxRate,
+        uint256 buyTax,     // in basis points (500 = 5%)
+        uint256 sellTax,
         uint256 burnRate,
         address devWallet,
-        uint256 maxTxPercentage,
+        uint256 maxTxPercent,
         bool renounce
     ) external payable returns (address) {
-        require(msg.value >= creationFee, "Insufficient creation fee");
-        require(buyTaxRate <= 1000, "Buy tax too high"); // Max 10%
-        require(sellTaxRate <= 1000, "Sell tax too high"); // Max 10%
-        require(burnRate <= 500, "Burn rate too high"); // Max 5%
-        require(maxTxPercentage <= 100, "Max tx too high");
+        require(msg.value >= creationFee, "Insufficient fee");
+        require(buyTax <= 1000 && sellTax <= 1000, "Tax too high");
+        require(burnRate <= 500, "Burn too high");
+
+        // Safely forward fee
+        (bool sent, ) = feeWallet.call{value: msg.value}("");
+        require(sent, "Fee transfer failed");
 
         CustomToken token = new CustomToken(
-            name,
-            symbol,
-            initialSupply,
-            decimals,
-            buyTaxRate,
-            sellTaxRate,
-            burnRate,
-            devWallet,
-            maxTxPercentage,
-            msg.sender
+            name, symbol, supply, decimals,
+            buyTax, sellTax, burnRate,
+            devWallet, maxTxPercent, msg.sender
         );
 
-        if (renounce) {
-            token.renounceOwnership();
-        } else {
-            token.transferOwnership(msg.sender);
-        }
+        if (renounce) token.renounceOwnership();
+        else token.transferOwnership(msg.sender);
 
         emit TokenCreated(address(token), msg.sender);
         return address(token);
@@ -67,44 +80,37 @@ contract CustomToken {
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
-    uint256 public buyTaxRate;
-    uint256 public sellTaxRate;
+
+    uint256 public buyTax;
+    uint256 public sellTax;
     uint256 public burnRate;
     address public devWallet;
     uint256 public maxTxAmount;
     address public owner;
     address public pair;
 
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
     constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _initialSupply,
-        uint8 _decimals,
-        uint256 _buyTaxRate,
-        uint256 _sellTaxRate,
-        uint256 _burnRate,
-        address _devWallet,
-        uint256 _maxTxPercentage,
-        address _owner
+        string memory _name, string memory _symbol, uint256 _supply, uint8 _decimals,
+        uint256 _buyTax, uint256 _sellTax, uint256 _burnRate,
+        address _devWallet, uint256 _maxTxPercent, address _owner
     ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimals;
-        totalSupply = _initialSupply * 10 ** _decimals;
-        buyTaxRate = _buyTaxRate;
-        sellTaxRate = _sellTaxRate;
+        totalSupply = _supply * (10 ** _decimals);
+        balanceOf[_owner] = totalSupply;
+        buyTax = _buyTax;
+        sellTax = _sellTax;
         burnRate = _burnRate;
         devWallet = _devWallet;
-        maxTxAmount = (_maxTxPercentage * totalSupply) / 100;
+        maxTxAmount = totalSupply * _maxTxPercent / 100;
         owner = _owner;
-        _balances[_owner] = totalSupply;
         emit Transfer(address(0), _owner, totalSupply);
     }
 
@@ -113,28 +119,21 @@ contract CustomToken {
         _;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
-        return _balances[account];
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 
-    function transfer(address to, uint256 amount) public returns (bool) {
+    function transfer(address to, uint256 amount) external returns (bool) {
         _transfer(msg.sender, to, amount);
         return true;
     }
 
-    function allowance(address owner_, address spender) public view returns (uint256) {
-        return _allowances[owner_][spender];
-    }
-
-    function approve(address spender, uint256 amount) public returns (bool) {
-        _approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
-        uint256 currentAllowance = _allowances[from][msg.sender];
-        require(currentAllowance >= amount, "Insufficient allowance");
-        _approve(from, msg.sender, currentAllowance - amount);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        if (allowance[from][msg.sender] != type(uint256).max) {
+            allowance[from][msg.sender] -= amount;
+        }
         _transfer(from, to, amount);
         return true;
     }
@@ -144,51 +143,35 @@ contract CustomToken {
     }
 
     function renounceOwnership() external onlyOwner {
-        emit OwnershipTransferred(owner, address(0));
         owner = address(0);
     }
 
     function transferOwnership(address newOwner) external onlyOwner {
-        require(newOwner != address(0), "Zero address");
-        emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
     }
 
     function _transfer(address from, address to, uint256 amount) internal {
-        require(from != address(0), "Zero address");
-        require(to != address(0), "Zero address");
-        require(_balances[from] >= amount, "Insufficient balance");
-        if (maxTxAmount > 0) {
-            require(amount <= maxTxAmount, "Exceeds max tx amount");
-        }
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        if (maxTxAmount > 0) require(amount <= maxTxAmount, "Exceeds max tx");
 
-        uint256 taxAmount = 0;
-        if (from == pair && buyTaxRate > 0) {
-            taxAmount = (amount * buyTaxRate) / 10000;
-        } else if (to == pair && sellTaxRate > 0) {
-            taxAmount = (amount * sellTaxRate) / 10000;
-        }
+        uint256 tax = 0;
+        if (from == pair && buyTax > 0) tax = amount * buyTax / 10000;
+        else if (to == pair && sellTax > 0) tax = amount * sellTax / 10000;
 
-        uint256 burnAmount = burnRate > 0 ? (amount * burnRate) / 10000 : 0;
-        uint256 transferAmount = amount - taxAmount - burnAmount;
+        uint256 burn = burnRate > 0 ? amount * burnRate / 10000 : 0;
+        uint256 send = amount - tax - burn;
 
-        _balances[from] -= amount;
-        _balances[to] += transferAmount;
-        if (taxAmount > 0 && devWallet != address(0)) {
-            _balances[devWallet] += taxAmount;
-            emit Transfer(from, devWallet, taxAmount);
-        }
-        if (burnAmount > 0) {
-            totalSupply -= burnAmount;
-            emit Transfer(from, address(0), burnAmount);
-        }
-        emit Transfer(from, to, transferAmount);
-    }
+        balanceOf[from] -= amount;
+        balanceOf[to] += send;
 
-    function _approve(address owner_, address spender, uint256 amount) internal {
-        require(owner_ != address(0), "Zero address");
-        require(spender != address(0), "Zero address");
-        _allowances[owner_][spender] = amount;
-        emit Approval(owner_, spender, amount);
+        if (tax > 0 && devWallet != address(0)) {
+            balanceOf[devWallet] += tax;
+            emit Transfer(from, devWallet, tax);
+        }
+        if (burn > 0) {
+            totalSupply -= burn;
+            emit Transfer(from, address(0), burn);
+        }
+        emit Transfer(from, to, send);
     }
 }
